506b39be48a06058ecfbd8aa43821111015589ed
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-11-07 00:24:15 +0100

    Fixed SASS requirement

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 const gulp = require('gulp');
-const sass = require('sass');
+const sass = require('gulp-sass')(require('sass'));
 
 gulp.task('watch', async () => {
-	gulp.src('./assets/src/**/*.scss')
-		.pipe(sass()).
-		.pipe(gulp.dest('./assets/dist'));
+	gulp.watch('./assets/src/css/**/*.scss', ['sass']);
+})
+
+gulp.task('sass', async () => {
+	return gulp.src('./assets/src/css/**/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('./assets/dist/css'));
 })
