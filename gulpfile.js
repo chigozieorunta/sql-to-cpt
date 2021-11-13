3c91cd6075187eaf19c36bda8de8e66b6ac967b3
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-11-13 20:31:49 +0100

    Added sass and minification to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,24 @@
 const {src, dest, watch} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const minifyCSS = require('gulp-clean-css');
+const browserSync = require('browser-sync').create();
 
 const compileCSS = () => {
 	return src('./assets/src/css/**/*.scss')
 		.pipe(sass())
-		.pipe(dest('./assets/dist/css'));
+		.pipe(minifyCSS())
+		.pipe(dest('./assets/dist/css'))
+		.pipe(browserSync.stream());
 }
 
 const watchCSS = () => {
+	browserSync.init({
+		server: {
+			baseDir: './'
+		}
+	})
 	watch('./assets/src/css/**/*.scss', compileCSS);
+	watch('./**/*.html').on('change', browserSync.reload);
 }
 
 exports.sass = compileCSS;
