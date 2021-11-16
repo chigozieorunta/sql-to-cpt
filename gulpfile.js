07ae9ef0b641385742b8574d4fea89dd2b729f40
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-11-16 13:27:23 +0100

    Added autoprefixer dev dep

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@ const {src, dest, watch} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const minifyCSS = require('gulp-clean-css');
 const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
 
 const compileCSS = () => {
 	return src('./assets/src/css/**/*.scss')
 		.pipe(sass())
+		.pipe(autoprefixer())
 		.pipe(minifyCSS())
 		.pipe(dest('./assets/dist/css'))
 		.pipe(browserSync.stream());
