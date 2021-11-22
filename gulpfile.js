cbb00152209be0d57606c9b5e3740d4806e9e657
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-11-22 23:34:00 +0100

    Added scripts workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 const { src, dest, watch } = require('gulp');
+
 const sass = require('gulp-sass')(require('sass'));
 const minify = require('gulp-clean-css');
-const browserSync = require('browser-sync').create();
 const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const merge = require('merge-stream');
 
 const compile = () => {
 	return src('./assets/src/css/**/*.scss')
@@ -13,6 +19,22 @@ const compile = () => {
 		.pipe(browserSync.stream());
 }
 
+const scripts = () => {
+	let core = src('./assets/src/js/**/*.js')
+		.pipe(babel())
+		.pipe(uglify())
+		.pipe(concat('scripts.js'))
+		.pipe(dest('./assets/dist/js'));
+
+	let jquery = src(['node_modules/jquery/dist/jquery.min.js'])
+		.pipe(concat('jquery.js'))
+		.pipe(dest('./assets/dist/js'));
+
+	return src('./assets/dist/js/**/*.js')
+		.pipe(concat('core.js'))
+		.pipe(dest('./assets/dist/js'));
+}
+
 const observe = () => {
 	browserSync.init({
 		server: {
@@ -20,8 +42,10 @@ const observe = () => {
 		}
 	})
 	watch('./assets/src/css/**/*.scss', compile);
+	watch('./assets/src/js/**/*.js', scripts);
 	watch('./**/*.html').on('change', browserSync.reload);
 }
 
 exports.sass = compile;
+exports.scripts = scripts;
 exports.watch = observe;
