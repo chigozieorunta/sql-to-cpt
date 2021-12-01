917533c950ede09d8f222a266a38bac1ae0be744
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-12-01 23:39:41 +0100

    Added plugin file

---
+++ src/Plugin.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Class
+ *
+ * @package SqlToCpt
+ */
+
+namespace SqlToCpt;
+
+/**
+ * Plugin entry point
+ */
+class Plugin {
+
+	/**
+	 * Constructor
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+
+	}
+}
